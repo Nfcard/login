6e932e57ac430bb0b6960acfaad41659998ca47c
Nfcard <godrive.management@gmail.com>
2024-08-18 10:22:36 +0600

    Update dension.js

--- dension.js
+++ dension.js
@@ -160,11 +160,11 @@ document.addEventListener("DOMContentLoaded", function() {
         if (profiles[accountNumber] && amount >= 0 && amount <= fetchedDataValue) {
             googleFormsData = [
                 {
-                    url: 'https://docs.google.com/forms/d/e/1FAIpQLSdwibAx-kNF8WUJMtkLovi5v7CvD8b331qg8cuIXxQgvBY3fQ/formResponse',
+                    url: 'https://docs.google.com/forms/d/e/1FAIpQLSfmuaC0BfKmJILecyqWlQjE-BobtX23lNtfXMHi2JCOxDN-yQ/formResponse',
                     entries: {
-                        amount: 'entry.571402887',
-                        description: 'entry.885732113'
-                    }
+                        amount: 'entry.1522107311',
+                        description: 'entry.1449208456'
+            }
                 },
                 {
                     url: surl,
